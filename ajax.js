00e61024b507a1cc73cb73a99481a891cd1535be
Tomas Rinblad <putzor@gmail.com>
2018-01-01 17:50:19 +0100

    Finished everything

--- ajax.js
+++ ajax.js
@@ -35,18 +35,13 @@ $.ajax({
 		}//End of employee
 		employees.push(employee);
 	});//End of each
-$(".Employee").on("click", function(e) {
-	$(this).toggleClass("Test");
-	$(this).find(".modal").toggle();
-	
-	}) //End of click
+	addEmployee();
 	} //End of sucess
-	
 }) //End of ajax
 
 function addEmployee() {
 	$.each(employees, function(i, employee){
-		let div = "<div class='Employee'>";
+		let div = "<div class='Employee' id='" + i + "'>";
 		div += "<div class='pic'></div>"
 		div += "<h2>" + employee.fullName;
 					  +"</h2>";
@@ -54,20 +49,45 @@ function addEmployee() {
 					  + "</p>";
 		div += "<p>"  + employee.location.state;
 					  + "</p>";
-		/*div += "<div class='modal'>"
-		div += "<p>"  +user.cell
-					  + "</p>"
-		div += "<p>"  + user.location.street
-					  + user.location.city
-					  + user.location.state
-					  + "</p>"
-		div += "<p>"  + "Birthday " + formatBoD(date);
-					  + "</p>"
-		div += "</div>";*/
 		div += "</div>";
-		$(".Employees").append(div);
+		$(".grid").append(div);
 		$(".Employee:last-child .pic").css("background-image", "url("+employee.picture+")");
-		//$(".modal").hide();
 		}) // End of Each
+	$(".Employee").on("click", function(e) {
+		id = $(this).attr("id");
+		modal(id);
+	
+	}) //End of click
 } //End of function
 
+function modal(i) {
+	let employee = employees[i];
+	let div = "<div class='modal-content' id='" + i + "'>";
+	let picture = employee.picture;
+	div += "<span class='close'>&times;</span>"
+	div += "<div class='pic modalpic' style='background-image: url("+picture+")'></div>"
+	div += "<div class='content'>"
+	div += "<h2>" + employee.fullName;
+				  +"</h2>";
+	div += "<p>"  + employee.email;
+				  + "</p>";
+	div += "<p>"  + employee.location.state;
+				  + "</p>";
+	div += "<hr>"
+	div += "<p>"  +employee.cell
+				  + "</p>"
+	div += "<p>"  + employee.location.street + " "
+				  + employee.location.city + " "
+				  + employee.location.state + " "
+				  + "</p>"
+	div += "<p>"  + "Birthday " + employee.birthDay;
+				  + "</p>"
+	div += "</div></div>";
+	$(".modal").append(div);
+	$(".modal").css("display", "block");
+	
+$(".close").click(function() {
+	$(".modal").css("display", "none");
+	$(".modal-content").remove()
+})
+}
